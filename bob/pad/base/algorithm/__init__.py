7b349d91280b44c733bbad2a82c97a6e4d6ccd2d
Pavel Korshunov <pavel.korshunov@idiap.ch>
2018-02-13 11:41:07 +0100

    moved SVM algorithm from bob.pad.face

--- bob/pad/base/algorithm/__init__.py
+++ bob/pad/base/algorithm/__init__.py
@@ -1,6 +1,27 @@
 from .Algorithm import Algorithm
+from .SVM import SVM
 
-# to fix sphinx warnings of not able to find classes, when path is shortened
-Algorithm.__module__ = "bob.pad.base.algorithm"
-# gets sphinx autodoc done right - don't remove it
+
+def __appropriate__(*args):
+    """Says object was actually declared here, and not in the import module.
+    Fixing sphinx warnings of not being able to find classes, when path is
+    shortened.
+
+    Parameters
+    ----------
+    *args
+        The objects that you want sphinx to believe that are defined here.
+
+    Resolves `Sphinx referencing issues <https//github.com/sphinx-
+    doc/sphinx/issues/3048>`
+    """
+
+    for obj in args:
+        obj.__module__ = __name__
+
+
+__appropriate__(
+    Algorithm,
+    SVM,
+)
 __all__ = [_ for _ in dir() if not _.startswith('_')]
