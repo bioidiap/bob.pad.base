ddb7d6e835d51afb6d07f1222525bd1041121007
Guillaume HEUSCH <guillaume.heusch@idiap.ch>
2018-02-28 13:44:57 +0100

    [test] yet another correction (previous one was sloppy) to test if array is numpy.array

--- bob/pad/base/test/test_algorithms.py
+++ bob/pad/base/test/test_algorithms.py
@@ -152,7 +152,6 @@ def test_convert_list_of_frame_cont_to_array():
   real_array = np.transpose(np.vstack([[random.gauss(mu, sigma) for _ in range(N)], [random.gauss(mu, sigma) for _ in range(N)]]))
 
   features_array = convert_list_of_frame_cont_to_array(real_array)
-  assert isinstance(features_array[0], np.array)
   assert type(features_array[0]) is np.array
   features_fm = convert_array_to_list_of_frame_cont(real_array)
   assert isinstance(features_fm[0], bob.bio.video.FrameContainer)
