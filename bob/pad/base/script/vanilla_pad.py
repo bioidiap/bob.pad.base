0ee60e5860c7e45f8a799c111b02de4f9a726448
Amir MOHAMMADI <amir.mohammadi@idiap.ch>
2020-12-04 17:04:31 +0100

    [vanilla-pad] Improve the dask client option

--- bob/pad/base/script/vanilla_pad.py
+++ bob/pad/base/script/vanilla_pad.py
@@ -1,6 +1,7 @@
 """Executes PAD pipeline"""
 
 
+from bob.bio.base.script.vanilla_biometrics import VALID_DASK_CLIENT_STRINGS
 import click
 from bob.extension.scripts.click_helper import ConfigCommand
 from bob.extension.scripts.click_helper import ResourceOption
@@ -37,9 +38,11 @@ from bob.extension.scripts.click_helper import verbosity_option
 @click.option(
     "--dask-client",
     "-l",
-    required=False,
-    cls=ResourceOption,
+    entry_point_group="dask.client",
+    string_exceptions=VALID_DASK_CLIENT_STRINGS,
+    default="single-threaded",
     help="Dask client for the execution of the pipeline.",
+    cls=ResourceOption,
 )
 @click.option(
     "--group",
@@ -78,6 +81,7 @@ def vanilla_pad(ctx, pipeline, database, dask_client, groups, output, checkpoint
     import bob.pipelines as mario
     import dask.bag
     from bob.extension.scripts.click_helper import log_parameters
+    from bob.pipelines.distributed.sge import get_resource_requirements
 
     logger = logging.getLogger(__name__)
     log_parameters(logger)
@@ -120,7 +124,8 @@ def vanilla_pad(ctx, pipeline, database, dask_client, groups, output, checkpoint
             pattern = f"{prefix}*{postfix}"
             os.makedirs(os.path.dirname(prefix), exist_ok=True)
             logger.info("Writing bag results into files ...")
-            result.to_textfiles(pattern, last_endline=True, scheduler=dask_client)
+            resources = get_resource_requirements(pipeline)
+            result.to_textfiles(pattern, last_endline=True, scheduler=dask_client, resources=resources)
 
             with open(scores_path, "w") as f:
                 # concatenate scores into one score file
