75ef5aab91b6e83496b9ef840f25afe35783daf6
Yannick DAYER <yannick.dayer@idiap.ch>
2021-06-14 17:12:58 +0200

    Add a test for the pad gen command

--- bob/pad/base/test/test_commands.py
+++ bob/pad/base/test/test_commands.py
@@ -2,6 +2,44 @@ from click.testing import CliRunner
 import pkg_resources
 from ..script import pad_commands
 from bob.extension.scripts.click_helper import assert_click_runner_result
+import os
+
+
+def test_gen_pad():
+    dev_ref_file = pkg_resources.resource_filename(
+        "bob.pad.base.test", "data/csv_scores/scores-dev.csv"
+    )
+    eval_ref_file = pkg_resources.resource_filename(
+        "bob.pad.base.test", "data/csv_scores/scores-eval.csv"
+    )
+    with open(dev_ref_file, "r") as f:
+        dev_ref = f.readlines()
+    with open(eval_ref_file, "r") as f:
+        eval_ref = f.readlines()
+    runner = CliRunner()
+    with runner.isolated_filesystem():
+        cwd = "./"
+        result = runner.invoke(
+            pad_commands.gen,
+            [
+                cwd,
+                "--mean-match",
+                "10",
+                "--mean-attacks",
+                "9",
+                "--mean-attacks",
+                "6",
+                "--n-attacks",
+                "2",
+            ],
+        )
+        assert_click_runner_result(result)
+        with open(os.path.join(cwd, "scores-dev.csv"), "r") as f:
+            for generated_line, reference in zip(f.readlines(), dev_ref):
+                assert generated_line == reference
+        with open(os.path.join(cwd, "scores-eval.csv"), "r") as f:
+            for generated_line, reference in zip(f.readlines(), eval_ref):
+                assert generated_line == reference
 
 
 def test_det_pad():
