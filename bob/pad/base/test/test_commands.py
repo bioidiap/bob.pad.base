955e102d27e31e362494889c4ffdc4eee22cbec0
Yannick DAYER <yannick.dayer@idiap.ch>
2021-06-14 17:14:41 +0200

    Change the tests score files input to CSV scores.

--- bob/pad/base/test/test_commands.py
+++ bob/pad/base/test/test_commands.py
@@ -43,74 +43,78 @@ def test_gen_pad():
 
 
 def test_det_pad():
-    licit_dev = pkg_resources.resource_filename('bob.pad.base.test',
-                                                'data/licit/scores-dev')
-    licit_test = pkg_resources.resource_filename('bob.pad.base.test',
-                                                 'data/licit/scores-eval')
-    runner = CliRunner()
-    with runner.isolated_filesystem():
-        result = runner.invoke(pad_commands.det, ['-e', '--output',
-                                                  'DET.pdf',
-                                                  licit_dev, licit_test])
-        assert_click_runner_result(result)
-
-
-def test_hist_pad():
-    licit_dev = pkg_resources.resource_filename('bob.pad.base.test',
-                                                'data/licit/scores-dev')
-    licit_test = pkg_resources.resource_filename('bob.pad.base.test',
-                                                 'data/licit/scores-eval')
-    spoof_dev = pkg_resources.resource_filename('bob.pad.base.test',
-                                                'data/spoof/scores-dev')
-    spoof_test = pkg_resources.resource_filename('bob.pad.base.test',
-                                                 'data/spoof/scores-eval')
-    runner = CliRunner()
-    with runner.isolated_filesystem():
-        result = runner.invoke(pad_commands.hist, [licit_dev])
-        assert_click_runner_result(result)
-
-    with runner.isolated_filesystem():
-        result = runner.invoke(pad_commands.hist, ['--criterion', 'min-hter',
-                                                   '--output',
-                                                   'HISTO.pdf', '-b',
-                                                   '30,20',
-                                                   licit_dev, spoof_dev])
-        assert_click_runner_result(result)
-
-    with runner.isolated_filesystem():
-        result = runner.invoke(pad_commands.hist, ['-e', '--criterion', 'eer',
-                                                   '--output',
-                                                   'HISTO.pdf', '-b', '30',
-                                                   licit_dev, licit_test,
-                                                   spoof_dev, spoof_test])
-        assert_click_runner_result(result)
-
-
-def test_metrics_pad():
-    licit_dev = pkg_resources.resource_filename('bob.pad.base.test',
-                                                'data/licit/scores-dev')
-    licit_test = pkg_resources.resource_filename('bob.pad.base.test',
-                                                 'data/licit/scores-eval')
+    dev = pkg_resources.resource_filename(
+        "bob.pad.base.test", "data/csv_scores/scores-dev.csv"
+    )
+    test = pkg_resources.resource_filename(
+        "bob.pad.base.test", "data/csv_scores/scores-eval.csv"
+    )
     runner = CliRunner()
     with runner.isolated_filesystem():
         result = runner.invoke(
-            pad_commands.metrics,
-            ['-e', licit_dev, licit_test]
+            pad_commands.det, ["-e", "--output", "DET.pdf", dev, test]
         )
         assert_click_runner_result(result)
 
 
-def test_evaluate_pad():
-    licit_dev = pkg_resources.resource_filename('bob.pad.base.test',
-                                                'data/licit/scores-dev')
-    licit_test = pkg_resources.resource_filename('bob.pad.base.test',
-                                                 'data/licit/scores-eval')
-    spoof_dev = pkg_resources.resource_filename('bob.pad.base.test',
-                                                'data/spoof/scores-dev')
-    spoof_test = pkg_resources.resource_filename('bob.pad.base.test',
-                                                 'data/spoof/scores-eval')
+def test_hist_pad():
+    dev = pkg_resources.resource_filename(
+        "bob.pad.base.test", "data/csv_scores/scores-dev.csv"
+    )
+    test = pkg_resources.resource_filename(
+        "bob.pad.base.test", "data/csv_scores/scores-eval.csv"
+    )
     runner = CliRunner()
     with runner.isolated_filesystem():
-        result = runner.invoke(pad_commands.evaluate,
-                               [licit_dev, licit_test, spoof_dev, spoof_test])
+        result = runner.invoke(pad_commands.hist, [dev])
+        assert_click_runner_result(result)
+
+    with runner.isolated_filesystem():
+        result = runner.invoke(
+            pad_commands.hist,
+            ["--criterion", "min-hter", "--output", "HISTO.pdf", "-b", "30,20", dev],
+        )
+        assert_click_runner_result(result)
+
+    with runner.isolated_filesystem():
+        result = runner.invoke(
+            pad_commands.hist,
+            [
+                "-e",
+                "--criterion",
+                "eer",
+                "--output",
+                "HISTO.pdf",
+                "-b",
+                "30",
+                dev,
+                test,
+            ],
+        )
+        assert_click_runner_result(result)
+
+
+def test_metrics_pad():
+    dev = pkg_resources.resource_filename(
+        "bob.pad.base.test", "data/csv_scores/scores-dev.csv"
+    )
+    test = pkg_resources.resource_filename(
+        "bob.pad.base.test", "data/csv_scores/scores-eval.csv"
+    )
+    runner = CliRunner()
+    with runner.isolated_filesystem():
+        result = runner.invoke(pad_commands.metrics, ["-e", dev, test, "-vvv"])
+        assert_click_runner_result(result)
+
+
+def test_evaluate_pad():
+    dev = pkg_resources.resource_filename(
+        "bob.pad.base.test", "data/csv_scores/scores-dev.csv"
+    )
+    test = pkg_resources.resource_filename(
+        "bob.pad.base.test", "data/csv_scores/scores-eval.csv"
+    )
+    runner = CliRunner()
+    with runner.isolated_filesystem():
+        result = runner.invoke(pad_commands.evaluate, [dev, test])
         assert_click_runner_result(result)
